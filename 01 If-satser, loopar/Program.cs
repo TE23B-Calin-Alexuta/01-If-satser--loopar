04732180aeb1cc51d1713f5fc810833aee91609e
Calin Alexuta <calin.alexuta@elev.ga.ntig.se>
2024-11-12 13:23:23 +0100

    first commit

---
+++ 01 If-satser, loopar/Program.cs
@@ -0,0 +1,89 @@
+﻿// string userInput;
+// int result;
+
+// do
+// {
+//     Console.Write("Skriv in ett heltal: ");
+//     userInput = Console.ReadLine();
+// } while (!int.TryParse(userInput, out result));
+
+// Console.WriteLine("Tack för att du skrev ett giltigt heltal.");
+
+
+
+
+
+
+
+
+
+//    Console.Write("Ange ditt användarnamn: ");
+//         string användarnamn = Console.ReadLine();
+
+//         if (användarnamn == "kalleanka")
+//         {
+//             Console.WriteLine("Welcome!");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Fel användarnamn!");
+//         }
+
+
+
+
+
+//  Console.Write("Ange ditt användarnamn: ");
+//         string användarnamn = Console.ReadLine();
+
+//         Console.Write("Ange ditt lösenord: ");
+//         string lösenord = Console.ReadLine();
+
+//         if (användarnamn == "kalleanka" && lösenord == "12345")
+//         {
+//             Console.WriteLine("Welcome!");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Wrong username or password");
+//         }
+
+
+
+
+
+
+
+
+        // int i = 0;
+        // while (i < 32)
+        // {
+        //     Console.WriteLine("Hello, World");
+        //     i++;
+        // }
+
+
+
+
+
+
+
+    //  string korrektLösenord = "SOBOLAN"; 
+    //     string användarensLösenord = ""; 
+    //     while (användarensLösenord != korrektLösenord)
+    //     {
+    //         Console.Write("Ange ditt lösenord: ");
+    //         användarensLösenord = Console.ReadLine();
+
+    //         if (användarensLösenord != korrektLösenord)
+    //         {
+    //             Console.WriteLine("Fel lösenord. Försök igen.");
+    //         }
+    //     }
+
+    //     Console.WriteLine("Welcome!"); 
+
+
+
+
+Console.ReadLine();
